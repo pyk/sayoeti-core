0ea244aa786efe3b180f842179957ff866eb869d
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-12-02 05:38:36 +0700

    dictionary: use AVL binary search tree

--- src/sayoeti.c
+++ src/sayoeti.c
@@ -34,65 +34,78 @@
 
 /****************************
  * Dictionary
- * We use binary search tree here. for fastest lookup and insert.
+ * We use AVL binary search tree here. for fastest search.
  ****************************/
 #define TRUE 1
 #define FALSE 0
+
 /* This is the maximum number of characters in one token.
  * see: https://en.wikipedia.org/wiki/Longest_Words#Indonesian */
 #define MAX_TOKEN_CHAR 31 
 
-/* dict_item represent a node of Binary Search Tree(BST) that we used as a 
- * underlying data structure for building dictionary.
- * Why? See https://en.wikipedia.org/wiki/Binary_search_tree */
+/* dict_item represents a node of AVL Binary Search Tree (BST) */
 struct dict_item {
-    /* basic info for each item in dictionary */
+    /* basic info for each item in dictionary; unique index and string term */
     long index;
     char *term;
 
-    /* for init & root dictionary; initializer node is to make sure that we only 
-     * select the root node only once in the insert operation */
-    int is_init;
-    int is_root;
-    
-    /* keep track of how many item in this dictionary. All item except the root
-     * will be zeroed */
-    long nitems;
-
-    struct dict_item *left, *right;
+    /* The height property for AVL tree */
+    int height;
+    struct dict_item *parent, *left, *right;
 };
 
-/* Initialize the root of dictionary */
-struct dict_item *dict_init(void)
+/* dictionary represents the dictionary */
+struct dictionary {
+    /* track how many items in the dictionary */
+    long nitems;
+
+    /* the root of dictionary */
+    struct dict_item *root;
+};
+
+/* Initialize the dictionary */
+struct dictionary *dict_init(void)
 {
-    struct dict_item *root = (struct dict_item *)malloc(sizeof(struct dict_item));
-    root->is_init = TRUE;
-    root->is_root = FALSE;
-    return root;
+    struct dictionary *dict = (struct dictionary *)malloc(sizeof(struct dictionary));
+    dict->nitems = 0;
+    dict->root = NULL;
+    return dict;
+}
+
+/* Print each item in the dictionary in alphabetical order */
+void dict_item_print(struct dict_item *item)
+{
+    if(item->left) dict_item_print(item->left);
+    printf("%li:%s\n", item->index, item->term);
+    if(item->right) dict_item_print(item->right);
 }
 
 /* Print all items in dictionary DICT */
-void dict_printout(struct dict_item *dict)
+void dict_printout(struct dictionary *dict)
 {
-    if (dict->is_root) {
-        printf("DICT: index: %li root: %s nitems: %li\n", dict->index, dict->term, dict->nitems);
-    }
-    if(dict->left) dict_printout(dict->left);
-    printf("%li:%s\n", dict->index, dict->term);
-    if(dict->right) dict_printout(dict->right);
+    printf("DICT: nitems: %li root: %s height: %d\n", dict->nitems, dict->root->term, dict->root->height);
+    dict_item_print(dict->root);
 }
 
-/* Destroy the dictionary DICT */
-void dict_destroy(struct dict_item *dict) 
+/* Destroy the dictionary item DICT */
+void dict_item_destroy(struct dict_item *item) 
 {
-    if(dict == NULL) {
+    if(item == NULL) {
         return;
     } 
 
-    dict_destroy(dict->left);
-    dict_destroy(dict->right);
+    dict_item_destroy(item->left);
+    dict_item_destroy(item->right);
 
-    free(dict->term);
+    free(item->term);
+    free(item);
+}
+
+/* Destroy dictionary DICT */
+void dict_destroy(struct dictionary *dict) 
+{ 
+    dict_item_destroy(dict->root);
+
     free(dict);
 }
 
@@ -100,42 +113,129 @@ void dict_destroy(struct dict_item *dict)
 struct dict_item *dict_item_new(char *term)
 {
     struct dict_item *item = (struct dict_item *)malloc(sizeof(struct dict_item));
-    item->left = item->right = NULL;
+    item->parent = NULL;
+    item->left = NULL;
+    item->right = NULL;
     item->index = 0;
     item->term = term;
-    item->is_init = FALSE;
-    item->is_root = FALSE;
-    item->nitems = 0;
+    item->height = 1;
+
     return item;
 }
 
-/* Insert dictionary item ITEM to a dictionary DICT. If the dictionary
- * is NULL then NODE will be the root of dictionary */
-struct dict_item *dict_item_insert(struct dict_item *dict, struct dict_item *item, int *is_new) 
+/* Choose the maximum value */
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+/* Return the height of item ITEM */
+int dict_item_height(struct dict_item *item)
 {
-    /* Insert to left or right item */
-    if(dict == NULL) {
-        *is_new = TRUE;
+    if(item == NULL) {
+        return 0;
+    }
+    return item->height;
+}
+
+/* Get the balance factor of node ITEM */
+int dict_item_get_balance(struct dict_item *item)
+{
+    if(item == NULL) {
+        return 0;
+    }
+    return dict_item_height(item->left) - dict_item_height(item->right);
+}
+
+/* Perform right rotation on node ITEM */
+struct dict_item *dict_item_rotate_right(struct dict_item *item)
+{
+    struct dict_item *backup_left = item->left;
+    struct dict_item *backup_right = backup_left->right;
+
+    /* Perform rotation */
+    backup_left->right = item;
+    item->left = backup_right;
+
+    /* Update the heights */
+    item->height = max(dict_item_height(item->left), dict_item_height(item->right)) + 1;
+    backup_left->height = max(dict_item_height(backup_left->left), dict_item_height(backup_left->right)) + 1;
+
+    /* return new root */
+    return backup_left;
+}
+
+/* Perform left rotation on node ITEM */
+struct dict_item *dict_item_rotate_left(struct dict_item *item)
+{
+    struct dict_item *backup_right = item->right;
+    struct dict_item *backup_left = backup_right->left;
+
+    /* Perform rotation */
+    backup_right->left = item;
+    item->right = backup_left;
+
+    /* Update the heights */
+    item->height = max(dict_item_height(item->left), dict_item_height(item->right)) + 1;
+    backup_right->height = max(dict_item_height(backup_right->left), dict_item_height(backup_right->right)) + 1;
+
+    /* return new root */
+    return backup_right;
+}
+
+/* Insert dictionary item ITEM to a dictionary root items ROOT. If the root
+ * is NULL then ITEM will be the root of the dictionary */
+struct dict_item *dict_item_insert(struct dict_item *root, struct dict_item *item) 
+{
+    /* Set root for the first time or assign to root->left or root->right */
+    if(root == NULL) {
         return item;
     }
 
-    /* Check if DICT is an initializer item; then make the item ITEM as a root
-     * of the dictionary */
-    if(dict->is_init) {
-        item->index = 1;
-        item->is_root = TRUE;
-        item->nitems = 1;
-        return item;
+    /* We compare each word in lexicographical order. left leaf start from 'a' */
+    if(strcmp(root->term, item->term) > 0) {
+        if(root->left == NULL) {
+            item->parent = root;
+        }
+        root->left = dict_item_insert(root->left, item);
+    } else if(strcmp(root->term, item->term) < 0) {
+        if(root->right == NULL) {
+            item->parent = root;
+        }
+        root->right = dict_item_insert(root->right, item);
     }
 
-    /* we compare each word in lexicographical order. left leaf start from 'a' */
-    if(strcmp(dict->term, item->term) > 0) {
-        dict->left = dict_item_insert(dict->left, item, is_new);
-    } else if(strcmp(dict->term, item->term) < 0) {
-        dict->right = dict_item_insert(dict->right, item, is_new);
+    /* Update the height of the ancestor node */
+    root->height = max(dict_item_height(root->left), dict_item_height(root->right)) + 1;
+    
+    /* Get the balance factor to check wether tree is balance or unbalanced */
+    int balance = dict_item_get_balance(root);
+
+    /* 4 case if the tree is unbalanced 
+     * Case 1: Left left case */
+    if(balance > 1 && (strcmp(root->left->term, item->term) > 0)) {
+        /* Perform right rotation */
+        return dict_item_rotate_right(root);
     }
 
-    return dict;
+    /* Case 2: Right right case */
+    if(balance < -1 && (strcmp(root->right->term, item->term) < 0)) {
+        /* Perform left rotation */
+        return dict_item_rotate_left(root);
+    }
+
+    /* Case 3: Left right case */
+    if(balance > 1 && (strcmp(root->left->term, item->term) < 0)) {
+        root->left = dict_item_rotate_left(root->left);
+        return dict_item_rotate_right(root);
+    }
+
+    /* Case 4: Right left case */
+    if(balance < -1 && (strcmp(root->right->term, item->term) > 0)) {
+        root->right = dict_item_rotate_right(root->right);
+        return dict_item_rotate_left(root);
+    }
+
+    return root;
 }
 
 /* Check ITEM if it exists in dictionary DICT */
@@ -159,7 +259,7 @@ int dict_item_exists(struct dict_item *dict, struct dict_item *item)
 
 /* Read each word from a file stream FP. Stem the word and insert the word
  * to the dictionary DICT. */
-struct dict_item *dict_build_from_file(struct dict_item *dict, FILE *fp)
+struct dictionary *dict_build_from_file(struct dictionary *dict, FILE *fp)
 {
     /* To save a discovered token temporary */
     char token[MAX_TOKEN_CHAR];
@@ -189,10 +289,17 @@ struct dict_item *dict_build_from_file(struct dict_item *dict, FILE *fp)
                 strcpy(t, token);
                 struct dict_item *vocab = dict_item_new(t);
                 
+                /* count root */
+                if(dict->root == NULL) {
+                    dict->nitems = 1;
+                }
                 /* Insert dictionary item VOCAB to a dictionary DICT */
-                int is_new_item = FALSE;
-                dict = dict_item_insert(dict, vocab, &is_new_item);
-                if(is_new_item) {
+                dict->root = dict_item_insert(dict->root, vocab);
+                /* for root vocab */
+                if(dict->nitems == 1) {
+                    vocab->index = dict->nitems;
+                }
+                if(vocab->parent) {
                     /* increment nitems, update vocab */
                     dict->nitems += 1;
                     vocab->index = dict->nitems;
@@ -282,21 +389,10 @@ int main(int argc, char** argv) {
     }
 
     /* Build stop words dictionary if the FILE is specified */
-    struct dict_item *stopwords_dict = NULL;
+    struct dictionary *stopwords_dict = NULL;
     if(opts.stopwords_file) {
         printf("sayoeti: build stop words dictionary from %s\n", opts.stopwords_file);
         stopwords_dict = dict_init();
-
-        /* Insert the first item to a stopwords dictionary; we choose mmmmm because m is 
-         * the 13th of alphabet. so we have nice binary search tree here */
-        struct dict_item *vocab = dict_item_new("mmmmm");
-        int is_new_item = FALSE;
-        stopwords_dict = dict_item_insert(stopwords_dict, vocab, &is_new_item);
-        if(is_new_item) {
-            /* Increment nitems, update vocab */
-            stopwords_dict->nitems += 1;
-            vocab->index = stopwords_dict->nitems;
-        }
 
         /* Read the file */
         FILE *fp = fopen(opts.stopwords_file, "r");
@@ -306,7 +402,7 @@ int main(int argc, char** argv) {
             exit(EXIT_FAILURE);
         }
 
-        /* Build dictionary from file */
+        /* Build dictionary from a file */
         stopwords_dict = dict_build_from_file(stopwords_dict, fp);
 
         /* Close the file; only display info if error */
@@ -319,17 +415,21 @@ int main(int argc, char** argv) {
     }
 
     if(stopwords_dict) {
-        struct dict_item *vocab = dict_item_new("tidaklah");
-        if(dict_item_exists(stopwords_dict, vocab)) {
+        struct dict_item *vocab = dict_item_new("tidass");
+        if(dict_item_exists(stopwords_dict->root, vocab)) {
             printf("%s\n", "EXISTS");
         } else {
             printf("%s\n", "NOT EXISTS");
         }
+        
+        // dict_printout(stopwords_dict);
     }
+
 
     printf("CORPUS = %s\n", opts.corpus_dir);
     printf("STOPWORDS = %s\n", opts.stopwords_file);
     printf("PORTS = %s\n", opts.port);
 
+    dict_destroy(stopwords_dict);
     return 0;
 }
